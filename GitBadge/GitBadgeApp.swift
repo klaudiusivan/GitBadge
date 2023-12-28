a795aaafd44fdd5735e818d03d2ad4bc4b9886b5
Klaudius ivan <klaudiusivanr>
2023-12-28 19:12:03 +0700

    Initial Commit

---
+++ GitBadge/GitBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBadgeApp.swift
+//  GitBadge
+//
+//  Created by Klaudius Ivan Anteraja on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
